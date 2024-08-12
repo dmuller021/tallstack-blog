6cbd482a51247a6cb57bc90cd892c24677d6617c
Dion Muller <d.muller@codefest.nl>
2024-08-12 12:52:47 +0200

    created blog page + card component

--- resources/views/blogs/blog.blade.php
+++ resources/views/blogs/blog.blade.php
@@ -1,3 +1,27 @@
 <x-app-layout>
-<h1>This is the blog page</h1>
+    <div class="flex justify-center">
+        <div class="mt-5 flex justify-center w-full">
+            <x-card url="https://google.com" title="Blog" message="This is an example"></x-card>
+        </div>
+    </div>
+
+    <div class="flex justify-center">
+        <div class="mt-5 flex justify-center w-full">
+            <x-card url="https://google.com" title="Blog" message="This is an example"></x-card>
+        </div>
+    </div>
+
+    <div class="flex justify-center">
+        <div class="mt-5 flex justify-center w-full">
+            <x-card url="https://google.com" title="Blog" message="This is an example"></x-card>
+        </div>
+    </div>
+
+    <div class="flex justify-center">
+        <div class="mt-5 flex justify-center w-full">
+            <x-card url="https://google.com" title="Blog" message="This is an example"></x-card>
+        </div>
+    </div>
 </x-app-layout>
+
+
